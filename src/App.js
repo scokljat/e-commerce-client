df63b56b9656ae3c3561651813441f3500a76919
scokljat <selma.cokljat@hotmail.com>
2023-03-30 23:39:59 +0200

    Added routes and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import { routes } from "./utils/Constants";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      {routes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.page} />
+      ))}
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/MyShop.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyShop() {
+  return <div>MyShop</div>;
+}
+
+export default MyShop;
