70e6656cb110f390031a225cd9f3808c5a0c2ba4
scokljat <selma.cokljat@hotmail.com>
2023-04-03 19:51:45 +0200

    Added login form and style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,51 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import { StyledButton } from "../globalStyle";
+import { StyledForm, StyledInput, ErrorDescription } from "./LoginStyle";
 
 function Login() {
-  return <div>Login</div>;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (values) => {
+    console.log(values);
+    reset();
+  };
+
+  return (
+    <StyledForm onSubmit={handleSubmit(onSubmit)}>
+      <StyledInput
+        placeholder="Email"
+        {...register("email", {
+          required: "Email is required",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "Please Enter A Valid Email!",
+          },
+        })}
+      />
+      {errors.email && (
+        <ErrorDescription>{errors.email.message}</ErrorDescription>
+      )}
+      <StyledInput
+        placeholder="Password"
+        {...register("password", {
+          required: "Password is required",
+          minLength: {
+            value: 8,
+            message: "Password must be at least 8 characters long!",
+          },
+        })}
+      />
+      {errors.password && (
+        <ErrorDescription>{errors.password.message}</ErrorDescription>
+      )}
+      <StyledButton type="submit">Login</StyledButton>
+    </StyledForm>
+  );
 }
 
 export default Login;
---
+++ src/pages/LoginStyle.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 40%;
+`;
+
+export const StyledInput = styled.input`
+  height: 40px;
+  border-radius: 5px;
+  border: 1px solid #6f6f6f;
+  padding: 0px 10px;
+  outline: none;
+`;
+export const ErrorDescription = styled.p`
+  color: #f0408d;
+  margin: 0;
+`;
