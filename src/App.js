d08cd7f2a4817075bff68af7e8a5597dd1c99752
scokljat <selma.cokljat@hotmail.com>
2023-04-04 09:09:37 +0200

    Updated global  style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { routes } from "./utils/Constants";
-import "./App.css";
 import NavBar from "./components/navbar/NavBar";
+import { MainContainer } from "./globalStyle";
+import "./App.css";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Routes>
-        {routes.map((route, index) => (
-          <Route key={index} path={route.path} element={route.page} />
-        ))}
-      </Routes>
+      <MainContainer>
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.page} />
+          ))}
+        </Routes>
+      </MainContainer>
     </>
   );
 }
