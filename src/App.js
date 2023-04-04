413c7fbbcf321f8975e5692e44077662824cfdba
scokljat <selma.cokljat@hotmail.com>
2023-04-04 10:04:50 +0200

    Added navigation if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { routes } from "./utils/Constants";
 import NavBar from "./components/navbar/NavBar";
 import { MainContainer } from "./globalStyle";
 import "./App.css";
 
 function App() {
+  const navigate = useNavigate();
+  const { isLoggedIn } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (isLoggedIn) navigate("/");
+  }, [isLoggedIn, navigate]);
+
   return (
     <>
       <NavBar />
