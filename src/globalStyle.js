0cec38a5390675511b03ac81be88ecf1b7c32d57
scokljat <selma.cokljat@hotmail.com>
2023-04-04 09:09:58 +0200

    Updated global style

---
+++ src/globalStyle.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  padding: 20px 0px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const StyledButton = styled.button`
+  background: #f0408d;
+  color: #fff;
+  padding: 10px;
+  border: 0;
+  border-radius: 5px;
+  cursor: pointer;
+`;
