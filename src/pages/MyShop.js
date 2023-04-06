7146c28c6fd19efc3e6a2d5b0da78e7281a286fd
scokljat <selma.cokljat@hotmail.com>
2023-04-06 20:28:32 +0200

    Added my shop

--- src/pages/MyShop.js
+++ src/pages/MyShop.js
@@ -1,7 +1,61 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Card from "../components/card/Card";
+import DeleteContent from "../components/deleteContent/DeleteContent";
+import { getUserProducts } from "../store/products/productSlice";
+import {
+  Description,
+  GridContainer,
+  Price,
+  PriceNumber,
+} from "./shop/ShopStyle";
+import { Wrapper } from "./shop/ShopStyle";
+import { StyledButton } from "../globalStyle";
 
 function MyShop() {
-  return <div>MyShop</div>;
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [productId, setProductId] = useState(0);
+  const dispatch = useDispatch();
+
+  const { userProducts } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(getUserProducts(1));
+  }, [dispatch]);
+
+  let totalPrice = userProducts?.reduce(function (prev, current) {
+    return prev + +current.product.price;
+  }, 0);
+
+  return (
+    <Wrapper>
+      {modalIsOpen && (
+        <DeleteContent setModalIsOpen={setModalIsOpen} productId={productId} />
+      )}
+      {!userProducts.length ? (
+        <Description>Nothing in this bag.</Description>
+      ) : (
+        <>
+          <GridContainer>
+            {userProducts.map((product) => (
+              <Card
+                key={product.id}
+                boughtProductId={product.id}
+                product={product.product}
+                size={product.size}
+                setModalIsOpen={setModalIsOpen}
+                setProductId={setProductId}
+              />
+            ))}
+          </GridContainer>
+          <Price>
+            PRICE IN TOTAL: <PriceNumber>{totalPrice} BAM</PriceNumber>
+          </Price>
+          <StyledButton style={{ width: "27.5%" }}>Buy</StyledButton>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default MyShop;
