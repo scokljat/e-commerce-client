bd110ce95719d1c60d4bc92139c2e828ce8f3dcd
scokljat <selma.cokljat@hotmail.com>
2023-04-06 20:11:17 +0200

    Added delete content

---
+++ src/components/deleteContent/DeleteContent.js
@@ -0,0 +1,36 @@
+import { useDispatch } from "react-redux";
+import { ReactComponent as Close } from "../../assets/images/bx-x.svg";
+import Modal from "../modal/Modal";
+import { Wrapper, ButtonContainer } from "./DeleteContentStyle";
+import { deleteBoughtProduct } from "../../store/boughtProducts/boughtProductsSlice";
+import { getUserById } from "../../store/user/userSlice";
+import { StyledButton } from "../../globalStyle";
+
+export default function DeleteContent({ setModalIsOpen, productId }) {
+  const dispatch = useDispatch();
+  return (
+    <Modal setModalIsOpen={setModalIsOpen}>
+      <Wrapper>
+        <Close
+          style={{ alignSelf: "flex-end", cursor: "pointer" }}
+          onClick={() => setModalIsOpen(false)}
+        />
+        <p>Are you sure you want to delete this product ?</p>
+        <ButtonContainer>
+          <StyledButton
+            onClick={() => {
+              dispatch(deleteBoughtProduct(productId)).then(() =>
+                dispatch(getUserById(1))
+              );
+            }}
+          >
+            Yes
+          </StyledButton>
+          <StyledButton onClick={() => setModalIsOpen(false)}>
+            Cancel
+          </StyledButton>
+        </ButtonContainer>
+      </Wrapper>
+    </Modal>
+  );
+}
---
+++ src/components/deleteContent/DeleteContentStyle.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+  padding: 30px;
+
+  p {
+    margin: 0;
+    font-size: 20px;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
