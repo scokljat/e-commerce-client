dc059eb0623a62704ed796f19d99900e78537898
scokljat <selma.cokljat@hotmail.com>
2023-04-11 22:06:13 +0200

    Added details folder

---
+++ src/pages/details/DetailsStyle.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+  width: 450px;
+  height: 570px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 70%;
+  gap: 80px;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Description = styled.p`
+  text-align: justify;
+`;
