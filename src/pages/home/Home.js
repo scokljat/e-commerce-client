acd2022519651598d6a6b0872b4039904de79006
scokljat <selma.cokljat@hotmail.com>
2023-05-15 19:56:35 +0200

    Added content to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,37 @@
-import { ButtonContainer } from "../../components/deleteContent/DeleteContentStyle";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ReactComponent as ArrowDown } from "../../assets/images/bx-down-arrow-alt.svg";
 import Slider from "../../components/slider/Slider";
-import { Wrapper, StyledButton } from "./HomeStyle";
+import Card from "../../components/card/Card";
+import { getFilteredProducts } from "../../store/products/productSlice";
+import { GridContainer, Description } from "../shop/ShopStyle";
+import { Wrapper, ArrowContainer, HomeSection, Title } from "./HomeStyle";
 
 function Home() {
+  const dispatch = useDispatch();
+  const { filteredProducts } = useSelector((state) => state.products);
+  useEffect(() => {
+    dispatch(getFilteredProducts("New"));
+  }, [dispatch]);
+
   return (
     <Wrapper>
       <Slider />
-      {/* <ButtonContainer>
-        <StyledButton>Shop now</StyledButton>
-        <StyledButton>Contact us</StyledButton>
-      </ButtonContainer> */}
+      <ArrowContainer href="#new">
+        <ArrowDown style={{ padding: "5px" }} />
+      </ArrowContainer>
+      <HomeSection id="new">
+        <Title>New products</Title>
+        {!filteredProducts?.length ? (
+          <Description>No products in this category.</Description>
+        ) : (
+          <GridContainer>
+            {filteredProducts?.map((product) => (
+              <Card product={product} key={product?.id} />
+            ))}
+          </GridContainer>
+        )}
+      </HomeSection>
     </Wrapper>
   );
 }
